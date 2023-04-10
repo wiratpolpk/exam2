addb7c106b8884ccb07cd378f031676d041f7179
Wiratpol <wiratpolpk@gmail.com>
2023-04-10 09:08:03 +0700

    update question

--- bin/exam2.dart
+++ bin/exam2.dart
@@ -2,5 +2,5 @@ import 'package:exam2/exam2.dart' as exam2;
 
 void main(List<String> arguments) {
   print('output1: ${exam2.computeInterestingTimes("15:15:00", "15:15:12")}');
-  print('output2: ${exam2.computeInterestingTimes("22:22:19", "22:22:24")}');
+  print('output2: ${exam2.computeInterestingTimes("22:22:21", "22:22:23")}');
 }
